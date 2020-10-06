8cfcd4e1a9113c99120f2108fb6c7ad3ab9d6275
Roger Dickey, Jr <rdickey@whytespyder.com>
2020-10-05 19:24:30 -0500

    Remove reference to 'clickme' component.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
-import Clickme, { click } from "./clickme"
 
 export default function About() {
 
   return (
       <Layout>
-            
-
-            <div>About me { click() }</div>
-
+            <div>About me</div>
             <img src="img/roger.jpeg" alt="Roger Dickey" className="img-thumbnail" />
-
-            <Clickme/>
-
-
       </Layout>
   )
 }
